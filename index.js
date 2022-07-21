040e1de1a2aa641b9cd56fa811f45ee1078d3a57
Chizzy250 <104734604+Chizzy250@users.noreply.github.com>
2022-07-22 00:47:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let input = document.querySelector("#todo");
+let btn = document.querySelector("#btn");
+let list = document.querySelector("#list");
+let savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+savedTasks.forEach(addItems);
+
+function addItems(text) {
+    let li = document.createElement("li")
+    li.innerHTML = text
+    list.insertBefore(li, list.childNodes[0])
+    
+    const delBtn = document.createElement("button")
+    delBtn.textContent = ""
+    delBtn.classList.add("fas", "fa-trash-alt")
+    li.appendChild(delBtn)
+    
+    delBtn.addEventListener("click", (e) => {
+        li.parentNode.removeChild(li);
+        savedTasks = savedTasks.filter ((e) => e !== text);
+        localStorage.setItem("tasks", JSON.stringify(savedTasks))
+    });
+}
+
+
+btn.addEventListener("click", () => {
+    let text =  input.value
+    if (text === "") {
+        alert("Hello, you didn't input anything.")
+    } else {
+        savedTasks.push(text)
+        localStorage.setItem("tasks", JSON.stringify(savedTasks))
+        input.value=""
+        addItems(text)
+    }
+});
+
+
+list.addEventListener("click", (e) => {
+    if (e.target.tagName == "LI") {
+        e.target.classList.toggle("done")
+    }
+});
